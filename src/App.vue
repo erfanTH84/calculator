ef7dd116b6f8b67e230adb3123307da870b35f93
erfanTH84 <98249774+erfanTH84@users.noreply.github.com>
2022-05-23 15:23:06 +0430

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <calculator></calculator>
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/calculator.vue";
+export default {
+  name: "App",
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style>
+body {
+  background: #000000 !important;
+}
+</style>
